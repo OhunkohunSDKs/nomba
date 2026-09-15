6977a9b16766bf01c1423d31a0c192874559c028
ohunkohun <hello@ohunkohun.com>
2026-09-15 21:24:13 +0100

    updated barrel exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Nomba } from "./resources/client.js";
 
 export type * from "./types/api-response.js";
 export type * from "./types/auth.js";
+export type * from "./types/bank.js";
+export type * from "./types/checkout.js";
 export type * from "./types/config.js";
 export type * from "./types/virtual-account.js";
-
